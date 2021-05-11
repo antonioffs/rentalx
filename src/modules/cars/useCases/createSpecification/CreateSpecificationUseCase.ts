63305c16fd3e05c8ecabc130925a2b3c258e525c
Felipe Silva <antonio.ffsmail@gmail.com>
2021-05-10 22:51:12 -0300

    Continuação do desenvolvimento

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,29 @@
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository"
+
+interface IRequest{
+    name: string;
+    description: string;
+};
+
+
+class CreateSpecificationUseCase{
+
+    constructor(private specificationsRepository: ISpecificationsRepository){
+    }
+
+    execute({name, description}: IRequest): void{
+        const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+        if(specificationAlreadyExists){
+            throw new Error("Specifications Already Exists!");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description
+        });
+    };
+
+}
+
+export { CreateSpecificationUseCase }

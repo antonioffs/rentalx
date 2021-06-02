782938051e309d0362f63967e1817c381d354f4a
Felipe Silva <antonio.ffsmail@gmail.com>
2021-06-02 07:30:30 -0300

    implementando a importacao de categorias de arquivos

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,19 +1,55 @@
 import fs from "fs";
 import csvParse from "csv-parse";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory{
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase{
+    constructor(private categoriesRepository: ICategoriesRepository){}
 
-    execute(file: Express.Multer.File): void{
-        const stream = fs.createReadStream(file.path);
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+    
+            const parseFile = csvParse();
+    
+            stream.pipe(parseFile);
+    
+            parseFile.on("data", async (line) => {
+                const[name, description] = line;
+                categories.push({
+                    name,
+                    description
+                });
+            })
+            .on("end", () => {
+                resolve(categories);
+            })
+            .on("error", (err) => {
+                reject(err)
+            });
+        });
+    };
 
-        const parseFile = csvParse();
+    async execute(file: Express.Multer.File): Promise<void>{
+        const categories = await this.loadCategories(file);
+        categories.map(async (category) => {
+            const {name, description} = category;
 
-        stream.pipe(parseFile)
+            const existCategory = this.categoriesRepository.findByName(name);
 
-        parseFile.on("data", async (line) => {
-            console.log(line)
-        })
-    }
+            if(!existCategory){
+                this.categoriesRepository.create({
+                    name,
+                    description
+                });
+            }
+        });
+    };
 }
 
 export { ImportCategoryUseCase }
